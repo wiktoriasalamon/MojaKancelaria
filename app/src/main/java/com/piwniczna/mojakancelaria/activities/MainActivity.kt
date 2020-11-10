819b7a7dbfc5e6db52cc8b613603d42ccde85bed
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-11-10 22:59:16 +0100

    feat(login): add LoginActivity and styles for Button and EditText

--- app/src/main/java/com/piwniczna/mojakancelaria/activities/MainActivity.kt
+++ app/src/main/java/com/piwniczna/mojakancelaria/activities/MainActivity.kt
@@ -1,11 +1,22 @@
-package com.piwniczna.mojakancelaria
+package com.piwniczna.mojakancelaria.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.piwniczna.mojakancelaria.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        finish()
     }
 }
