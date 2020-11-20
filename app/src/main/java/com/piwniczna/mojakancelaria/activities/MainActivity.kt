b18219474c3f630bb19a42c5a2015f05ac259d7a
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-11-20 13:17:43 +0100

    feat(ClientsList): add circle button and search input style; create ClientsListFragment

--- app/src/main/java/com/piwniczna/mojakancelaria/activities/MainActivity.kt
+++ app/src/main/java/com/piwniczna/mojakancelaria/activities/MainActivity.kt
@@ -3,11 +3,18 @@ package com.piwniczna.mojakancelaria.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.piwniczna.mojakancelaria.R
+import com.piwniczna.mojakancelaria.activities.clients.ClientsListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                ClientsListFragment()
+        ).commit()
+
     }
 
     override fun onStop() {
