0557252f48fd0398b66c6cfb25a2965f1a56d8b7
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-11-25 23:33:06 +0100

    feat(filter-clients): add filter to ClientsListAdapter

--- app/src/main/java/com/piwniczna/mojakancelaria/activities/clients/ClientsFragment.kt
+++ app/src/main/java/com/piwniczna/mojakancelaria/activities/clients/ClientsFragment.kt
@@ -2,11 +2,14 @@ package com.piwniczna.mojakancelaria.activities.clients
 
 import android.R.attr.button
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.piwniczna.mojakancelaria.R
@@ -17,18 +20,39 @@ class ClientsFragment : Fragment() {
     lateinit var clientsListAdapter: ClientsListAdapter
     lateinit var clientsListView : ListView
     lateinit var clientsList: ArrayList<String>
+    lateinit var searchClientsEditText: EditText
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_clients, container, false)
 
-        val addButton = view.findViewById<Button>(R.id.addClientButton)
+        val addButton = view.findViewById<Button>(R.id.add_client_button)
         addButton.setOnClickListener {handleAddClient(it)}
 
         clientsListView = view.findViewById(R.id.clients_list_view) as ListView
         clientsList = arrayListOf()
         clientsListAdapter = ClientsListAdapter(this.context!!, clientsList)
         clientsListView.adapter = clientsListAdapter
+
+        searchClientsEditText = view.findViewById(R.id.search_clients_edittext)
+        searchClientsEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if(s != "") {
+                    Log.e("CLIENTS", s.toString())
+                    clientsListAdapter.filter.filter(s)
+                } else {
+
+                }
+            }
+        })
 
         getClients()
         clientsListAdapter.notifyDataSetChanged()
