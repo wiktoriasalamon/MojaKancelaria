7d14d8a214a65f208f6c0cbb4462b62983f01453
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-11-25 20:12:13 +0100

    feat(delete-client): delete client after long click on list item

--- app/src/main/java/com/piwniczna/mojakancelaria/activities/clients/ClientsFragment.kt
+++ app/src/main/java/com/piwniczna/mojakancelaria/activities/clients/ClientsFragment.kt
@@ -1,33 +1,71 @@
 package com.piwniczna.mojakancelaria.activities.clients
 
+import android.app.AlertDialog
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.piwniczna.mojakancelaria.R
+import com.piwniczna.mojakancelaria.utils.SpannedText
+import java.text.FieldPosition
 
 class ClientsFragment : Fragment() {
     lateinit var clientsListAdapter: ClientsListAdapter
     lateinit var clientsListView : ListView
-    lateinit var clientsList: List<String>
+    lateinit var clientsList: ArrayList<String>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_clients, container, false)
         clientsListView = view.findViewById(R.id.clients_list_view) as ListView
-        clientsList = listOf()
+        clientsList = arrayListOf()
         clientsListAdapter = ClientsListAdapter(this.context!!, clientsList)
         clientsListView.adapter = clientsListAdapter
 
+        clientsListView.setOnItemLongClickListener { _, _, position, id ->
+            deleteClient(position, id)
+            true
+        }
+
         getClients()
-        clientsListAdapter.notifyDataSetChanged()
 
         return view
     }
 
-    fun getClients(): List<String> {
-        // TODO: get clients from db
-        return listOf("Jan Kowalski", "Anna Nowak", "Katarzyna Nosowska")
+    private fun getClients() {
+        val clients = arrayListOf("Jan Kowalski", "Anna Nowak", "Katarzyna Nosowska")
+        clientsList.clear()
+        clientsList.addAll(clients)
+        clientsListAdapter.notifyDataSetChanged()
     }
+
+    private fun deleteClient(position: Int, id: Long) {
+        val builder = AlertDialog.Builder(this.context)
+
+        val clientName = clientsListAdapter.data[position]
+        val message = SpannedText.getSpannedText(getString(R.string.delete_client, clientName))
+
+        builder.setTitle(R.string.warning)
+        builder.setMessage(message)
+
+        builder.setPositiveButton(R.string.delete) { dialog, which ->
+            deleteClientFromDB(id.toString())
+        }
+
+        builder.setNegativeButton(R.string.cancel) { dialog, which -> }
+
+        builder.show()
+    }
+
+    private fun deleteClientFromDB(id: String) {
+        Log.e("Client", "deleteing client ${id} ...")
+        // TODO: delete client from database
+        getClients()
+    }
+
 }
