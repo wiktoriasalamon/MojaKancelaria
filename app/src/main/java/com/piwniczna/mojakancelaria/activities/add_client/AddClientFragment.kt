c99369abc8db021b84bc588732aadc937e798d46
sasuke5055 <maciek.lewandowicz@gmail.com>
2020-11-26 16:02:04 +0100

    feat(ClientFragment): Add client to db, delete and get clients from db

--- app/src/main/java/com/piwniczna/mojakancelaria/activities/add_client/AddClientFragment.kt
+++ app/src/main/java/com/piwniczna/mojakancelaria/activities/add_client/AddClientFragment.kt
@@ -1,5 +1,6 @@
 package com.piwniczna.mojakancelaria.activities.add_client
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,16 +10,20 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.piwniczna.mojakancelaria.DB.DataService
+import com.piwniczna.mojakancelaria.Models.ClientEntity
 import com.piwniczna.mojakancelaria.R
 import com.piwniczna.mojakancelaria.activities.clients.ClientsFragment
 import kotlinx.android.synthetic.main.fragment_add_client.*
 
 class AddClientFragment : Fragment() {
     lateinit var clientEditText : EditText
+    lateinit var dbService: DataService
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_add_client, container, false)
 
+        dbService = DataService(this.context!!)
         val addButton = view.findViewById<Button>(R.id.saveClientButton)
         addButton.setOnClickListener {handleSaveClient(it)}
 
@@ -28,21 +33,26 @@ class AddClientFragment : Fragment() {
     }
 
     fun handleSaveClient(view: View) {
-        val newClient = newClientEditText.text.toString()
-        if (newClient == "") {
+        val newClientName = newClientEditText.text.toString()
+        if (newClientName == "") {
             val text = R.string.empty_client_warning
             val duration = Toast.LENGTH_LONG
             val toast = Toast.makeText(activity?.applicationContext, text, duration)
             toast.show()
-        } else {
-            // TODO: add client to database
-            Log.e("ADD CLIENT", newClient)
-            fragmentManager?.beginTransaction()?.replace(
-                R.id.fragment_container,
-                ClientsFragment()
-            )?.commit()
+            return
         }
 
+        addNewClientToDB(ClientEntity(newClientName))
+
+        fragmentManager?.beginTransaction()?.replace(
+            R.id.fragment_container,
+            ClientsFragment()
+        )?.commit()
+
+    }
+
+    fun addNewClientToDB(client: ClientEntity){
+        AsyncTask.execute { dbService.addClient(client) }
     }
 
 }
