a94c12d8dce8af0133c3c366ca8319e5a130991a
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-11-26 18:08:19 +0100

    feat(client-details): open ClientDetailsFragment after click on client list item

--- app/src/main/java/com/piwniczna/mojakancelaria/activities/client_details/ClientDetailsFragment.kt
+++ app/src/main/java/com/piwniczna/mojakancelaria/activities/client_details/ClientDetailsFragment.kt
@@ -1,6 +1,5 @@
-package com.piwniczna.mojakancelaria.activities.add_client
+package com.piwniczna.mojakancelaria.activities.client_details
 
-import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,17 +7,28 @@ import android.view.View
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import com.piwniczna.mojakancelaria.DB.DataService
+import android.widget.TextView
 import com.piwniczna.mojakancelaria.Models.ClientEntity
 import com.piwniczna.mojakancelaria.R
 import com.piwniczna.mojakancelaria.activities.clients.ClientsFragment
-import kotlinx.android.synthetic.main.fragment_add_client.*
 
-class ClientDetailsFragment : Fragment() {
+class ClientDetailsFragment(var client: ClientEntity) : Fragment() {
+    lateinit var titleTextView: TextView
+    lateinit var obligationsButton: Button
+    lateinit var paymentsButton: Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_client_details, container, false)
+
+        titleTextView = view.findViewById(R.id.client_details_title)
+        titleTextView.text = client.name
+
+        obligationsButton = view.findViewById(R.id.obligations_button)
+        obligationsButton.setOnClickListener { openObligationsFragment(it) }
+
+        paymentsButton = view.findViewById(R.id.payments_button)
+        paymentsButton.setOnClickListener { openPaymentsFragment(it) }
+
         return view
     }
 
@@ -27,5 +37,15 @@ class ClientDetailsFragment : Fragment() {
             R.id.fragment_container,
             ClientsFragment()
         )?.commit()
+    }
+
+    private fun openObligationsFragment(view: View) {
+        //TODO open obligations fragment with client as a parameter
+        Log.e("DETAILS", "obligations")
+    }
+
+    private fun openPaymentsFragment(view: View) {
+        //TODO open payments fragment with client as a parameter
+        Log.e("DETAILS", "payments")
     }
 }
