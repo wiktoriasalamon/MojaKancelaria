350f73216fe6f9c237b94cde4e602ef79820ae05
sasuke5055 <42801480+sasuke5055@users.noreply.github.com>
2020-12-13 04:18:25 +0100

    Report button (#93)
    * feat(layout cases): add buttons for archiving and sending report
    
    * feat(icons): add icons to new buttons
    
    * feat(report): send email with text version

--- app/src/main/java/com/piwniczna/mojakancelaria/activities/cases/case_details/CaseDetailsFragment.kt
+++ app/src/main/java/com/piwniczna/mojakancelaria/activities/cases/case_details/CaseDetailsFragment.kt
@@ -1,24 +1,35 @@
 package com.piwniczna.mojakancelaria.activities.cases.case_details
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.piwniczna.mojakancelaria.Models.CaseEntity
 import com.piwniczna.mojakancelaria.Models.ClientEntity
 import com.piwniczna.mojakancelaria.R
 import com.piwniczna.mojakancelaria.activities.cases.cases_list.CasesFragment
 import com.piwniczna.mojakancelaria.activities.cases.ObligationsFragment
 import com.piwniczna.mojakancelaria.activities.payments.payments_list.PaymentsFragment
+import com.piwniczna.mojakancelaria.utils.EmailSender
+import com.piwniczna.mojakancelaria.utils.ReportGenerator
 
 class CaseDetailsFragment(var client: ClientEntity, var case: CaseEntity) : Fragment() {
     lateinit var clientTextView: TextView
     lateinit var caseTextView: TextView
     lateinit var obligationsButton: Button
     lateinit var paymentsButton: Button
+    lateinit var archiveButton: Button
+    lateinit var reportButton: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_case_details, container, false)
@@ -35,7 +46,25 @@ class CaseDetailsFragment(var client: ClientEntity, var case: CaseEntity) : Frag
         paymentsButton = view.findViewById(R.id.payments_button)
         paymentsButton.setOnClickListener { openPaymentsFragment(it) }
 
+        archiveButton = view.findViewById(R.id.archive_button)
+        archiveButton.setOnClickListener { archiveCase(it) }
+
+        reportButton = view.findViewById(R.id.report_button)
+        reportButton.setOnClickListener { sendReport(it) }
+
         return view
+    }
+
+    private fun sendReport(view: View) {
+        //todo
+        AsyncTask.execute {
+            var reports = ReportGenerator.generateReport(case, this.context!!)
+            EmailSender.sendEmail(this.context!!, reports[0], reports[1], "test@test.com")
+        }
+    }
+
+    private fun archiveCase(view: View) {
+        //todo
     }
 
     fun onBackPressed() {
@@ -58,4 +87,5 @@ class CaseDetailsFragment(var client: ClientEntity, var case: CaseEntity) : Frag
                 PaymentsFragment(client, case)
         )?.commit()
     }
+
 }
